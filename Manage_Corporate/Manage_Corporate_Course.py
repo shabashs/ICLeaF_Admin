cb12b7e904c47b1fba46104025b168bd5ba9d95d
shabashs <shabash.s@infocareerindia.com>
2024-06-21 16:22:17 +0530

    21-jun

--- Manage_Corporate/Manage_Corporate_Course.py
+++ Manage_Corporate/Manage_Corporate_Course.py
@@ -1,5 +1,5 @@
 import configparser
-from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from Manage_pack.components import components
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,12 +9,10 @@ import time
 config = configparser.RawConfigParser()
 config.read('Manage_Pack/my.properties')
 
-
 class Manage_Corporate_Course:
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(self.driver, 60)
-
+        self.wait = WebDriverWait(self.driver, 30)
 
     def Config_Course(self):
         self.wait.until(EC.element_to_be_clickable((By.XPATH, config.get('ok', 'Manage_corporate_Course')))).click()
@@ -28,8 +26,10 @@ class Manage_Corporate_Course:
         element = self.wait.until(EC.element_to_be_clickable((By.XPATH, config.get('ok', 'confi'))))
         actions = ActionChains(self.driver)
         actions.move_to_element(element).click().perform()
-        #Manage_Corporate_Course.Config_Course_1(self)
+        Manage_Corporate_Course.Config_Course_1(self)
         Manage_Corporate_Course.Config_Course_2(self)
+        Manage_Corporate_Course.preview(self)
+        Manage_Corporate_Course.delete_page(self)
 
     def Config_Course_1(self):
         self.wait.until(EC.element_to_be_clickable((By.XPATH, config.get('ok', 'plus')))).click()
@@ -66,19 +66,27 @@ class Manage_Corporate_Course:
         self.wait.until(EC.visibility_of_element_located((By.XPATH,config.get('ok','details'))))
         self.driver.save_screenshot("E:/project/Screenshort/config_session_1.png")
         self.wait.until(EC.element_to_be_clickable((By.XPATH,config.get('ok','okay_button')))).click()
+
+    def preview(self):
+        time.sleep(3)
         self.wait.until(EC.element_to_be_clickable((By.XPATH,config.get('ok','preview_button')))).click()
-        #self.wait.until(EC.element_to_be_clickable((By.XPATH,config.get('ok','subject_q')))).click()
-        #self.wait.until(EC.element_to_be_clickable((By.XPATH,config.get('ok','open_pdf')))).click()
-        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-        next =self.wait.until(EC.visibility_of_element_located((By.XPATH,config.get('ok','next_button'))))
-        actions = ActionChains(self.driver)
-        actions.move_to_element(next).click().perform()
-        self.wait.until(EC.element_to_be_clickable((By.XPATH,config.get('ok','close_icon')))).click()
+        new_tab_handle = self.driver.window_handles[-1]
+        self.driver.switch_to.window(new_tab_handle)
+        self.wait.until(EC.element_to_be_clickable((By.XPATH,config.get('ok','next_button')))).click()
+        self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,config.get('ok','close_icon')))).click()
         self.wait.until(EC.visibility_of_element_located((By.XPATH,config.get('ok','close_window'))))
-
         self.wait.until(EC.element_to_be_clickable((By.XPATH,config.get('ok','Yes_button')))).click()
+        time.sleep(3)
+        try:
+            self.driver.close() # don't touch this line if u remove the code won't handle the Preview
+            self.driver.switch_to.window(self.driver.window_handles[0])
+        except Exception as e:
+            pass
 
-
-
-
-
+    def delete_page(self):
+        element_to_hover = self.wait.until(EC.visibility_of_element_located((By.XPATH, config.get('ok', 'mouse'))))
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element_to_hover).perform()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, config.get('ok', 'penile')))).click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, config.get('ok', 'first_delete')))).click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, config.get('ok', 'Yes_button')))).click()
